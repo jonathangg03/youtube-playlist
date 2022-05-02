c9a2c00178947ca7ca7d9b2464cb77d9e0356071
Jonathan García <megabyte9703@gmail.com>
2022-05-02 16:03:08 -0600

    Add: delete video from video list

--- src/components/App/index.js
+++ src/components/App/index.js
@@ -25,7 +25,12 @@ const App = () => {
   }
 
   const handleOnDragEnd = (result) => {
-    setPlaylistVideos((prev) => prev.concat(result.draggableId))
+    if (result.destination.droppableId !== result.source.droppableId) {
+      setItems((prev) =>
+        prev.filter((el) => el.id.videoId !== result.draggableId)
+      )
+      setPlaylistVideos((prev) => prev.concat(result.draggableId))
+    }
   }
 
   return (
