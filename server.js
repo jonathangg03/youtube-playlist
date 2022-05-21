0bd2fa4a411d46a26faa7c381261ab14c8357193
Jonathan García <jona03g97@gmail.com>
2022-05-21 17:49:54 -0600

    Add: uncomment set port

--- server.js
+++ server.js
@@ -3,7 +3,7 @@ const cors = require('cors')
 const path = require('path')
 const app = express()
 
-// app.set('port', process.env.PORT)
+app.set('port', process.env.PORT)
 
 app.use(cors())
 app.use(express.static(path.join(__dirname, '/public/dist')))
